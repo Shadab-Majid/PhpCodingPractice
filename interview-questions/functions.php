22c6f7815bed6741596f1384d08124328b1adb3d
Shadab-Majid <134842357+Shadab-Majid@users.noreply.github.com>
2025-10-22 11:45:52 +0000

    leetcodeproblem

--- interview-questions/functions.php
+++ interview-questions/functions.php
@@ -52,4 +52,75 @@ function removeDuplicate(array $arr) : array {
 }
 
 
+function square_even_idx_values($arr) {
 
+    $n = count($arr);
+    if($n === 1) {
+        return false;
+    }
+
+    for($i = 0; $i < $n; $i++) {
+        if($i % 2 == 0) {
+            $arr[$i] = $arr[$i] * $arr[$i];
+        }
+    }
+    return $arr;
+}
+
+
+//time complaxity = O(n)
+// space complaxity = O(n)
+function evenFollowedByOddUsingNewArray(array $arr) : array {
+    
+}
+
+
+// function to show even array elements followed by odd numbers best approch
+//time complaxity = O(n)
+//space complaxity = O(1)
+
+function evenFollowedByOdd(array $arr) : array {
+
+    if(count($arr) === 1) {
+        return false;
+    }
+
+    // double pointer approch
+    $i = 0;
+    $j = count($arr) - 1;
+    echo $j;
+    $temp = 0;
+    while ($i < $j) {
+        while($i < $j && $arr[$i] % 2 == 0) $i++;
+        while($i < $j && $arr[$j] % 2 != 0) $j--;
+
+        if($i < $j) {
+            $temp = $arr[$i];
+            $arr[$i] = $arr[$j];
+            $arr[$j] = $temp;
+            $i++;
+            $j--;
+        }
+
+    }
+    return $arr;
+}
+
+
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+//Input: nums = [2,7,11,15], target = 9
+//Output: [0,1]
+//Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+function twoSum(array $nums, int $target) : array {
+    print_r($nums);
+    $output = [];
+    foreach($nums as $key => $num) {
+        $compliment = $target - $num;
+        $compliment_idx = array_search($compliment, $nums);
+        if($compliment_idx != false) {
+            return $output[] = $compliment_idx;
+        }
+        $output[] = $key;
+    }
+}
--- interview-questions/input.php
+++ interview-questions/input.php
@@ -8,13 +8,24 @@ $fibonacci_series = fibonacciSeries($nums);
 // echo $fibonacci_series;
 
 $colmn_and_row_num = 10;
-// $pattern = tringle($colmn_and_row_num);
+$pattern = tringle($colmn_and_row_num);
 
 
 $str = 'Shadab Majid';
 $reverse_word = revervseStringByWord($str);
 // echo $reverse_word;
 
-$arr = [1,2,3,2,5,6];
+$arr = [2,2,3,2,5,6];
 
-var_dump(removeDuplicate($arr));
+// var_dump(removeDuplicate($arr));
+$square_even_idx_values  = square_even_idx_values($arr);
+// print_r($square_even_idx_values);
+
+// $evenFollowedByOdd = evenFollowedByOdd($arr);
+$nums = [2,7,11,15];
+$target = 9;
+
+twoSum($nums, $target);
+
+
+// print_r($evenFollowedByOdd);
